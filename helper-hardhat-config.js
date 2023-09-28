bc375ab6d687db60dec90246ba3f66295aa313dd
Nikhil8400 <134049704+Nikhil8400@users.noreply.github.com>
2023-09-28 13:06:29 +0530

    Update helper-hardhat-config.js

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -2,7 +2,7 @@ const { ethers } = require("hardhat")
 
 const networkConfig = {
     11155111: {
-        name: "seplia",
+        name: "sepolia",
         vrfCoordinatorV2: "0x8103b0a8a00be2ddc778e6e7eaa21791cd364625",
         raffleEntranceFee: ethers.utils.parseEther("0.01"),
         gasLane: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c",
